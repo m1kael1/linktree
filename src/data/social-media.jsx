0b9f281ec7ff0fc88ae3da7badf77af13edfac38
m1kael1 <mikaelagungdjawa@gmail.com>
2023-09-06 20:38:53 +0700

    add link

--- src/data/social-media.jsx
+++ src/data/social-media.jsx
@@ -9,14 +9,14 @@ const links = [
 	},
 	{
 		social_media: "Linkedin",
-		link: "https://github.com/m1kael1",
+		link: "https://www.linkedin.com/in/mikaelagung",
 		add_class_name:
 			" hover:bg-gradient-to-l bg-gradient-to-r from-blue-700 to-blue-500  ",
 		icon: <GrLinkedin size={25} />,
 	},
 	{
 		social_media: "Instagram",
-		link: "https://github.com/m1kael1",
+		link: "https://www.instagram.com/mikaelagng",
 		add_class_name:
 			" bg-gradient-to-r  from-[#833AB4] via-[#FD1D1D] to-[#F77737] hover:bg-gradient-to-l ",
 		icon: <GrInstagram size={25} />,
